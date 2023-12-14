b2dec986bbff5cf35b4df5f6b5a9227d7212a860
Tim Huitt <timhuitt@gmail.com>
2023-12-14 01:05:03 -0700

    add header tab current page indication

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import LogoSvg from '../../assets/logo.jsx'
 
 import "./Header.css"
 
 const Header = () => {
+  const loc = useLocation().pathname
   return (
     <nav className='Header'>
       <div className='logo'>
         <LogoSvg />
       </div>
-      <Link to="/projects">Projects</Link>
-      <Link to="/resume">Resume</Link>
-      <Link to="/contact">Contact</Link>
+      <Link 
+        to="/projects" 
+        className={loc==='/projects' ? 'selected' : ''}
+      >
+        Projects
+      </Link>
+      <Link 
+        to="/resume" 
+        className={loc==='/resume' ? 'selected' : ''}
+      >
+        Resume
+      </Link>
+      <Link 
+        to="/contact" 
+        className={loc==='/contact' ? 'selected' : ''}
+      >
+        Contact
+      </Link>
     </nav>
   );
 };
