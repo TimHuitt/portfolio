f9960d1de3968d83975a8cb72454a0737663ee13
Tim Huitt <timhuitt@gmail.com>
2023-12-14 14:01:50 -0700

    update server for deployment

--- server/config/db.connection.js
+++ server/config/db.connection.js
@@ -1,10 +1,26 @@
+const express = require('express');
 const mongoose = require('mongoose');
-const {DATABASE_URI} = process.env
+const { DATABASE_URI } = process.env;
+
 mongoose.set('strictQuery', true);
-mongoose.connect(DATABASE_URI)
+mongoose.connect(DATABASE_URI);
+
+// Express app
+const app = express();
+
+// Serve static files (replace 'public' with the actual directory where your static files are located)
+app.use(express.static('public'));
+
+// Additional middleware and routes go here...
 
 // Connection Events
 mongoose.connection
-  .on("open", () => console.log("mongoose connected!"))
-  .on("close", () => console.log("mongoose disconnected!"))
-  .on("error", (error) => console.log(error));
+  .on('open', () => console.log('mongoose connected!'))
+  .on('close', () => console.log('mongoose disconnected!'))
+  .on('error', (error) => console.log(error));
+
+// Start the server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
