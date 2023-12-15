d99150c2adb8222a0c31222e101b2cd697d8c2b0
Tim Huitt <timhuitt@gmail.com>
2023-12-14 22:50:24 -0700

    update pages, styling, content, add pdf download

--- client/src/components/ProjectCard/ProjectCard.jsx
+++ client/src/components/ProjectCard/ProjectCard.jsx
@@ -4,15 +4,14 @@ import "./ProjectCard.css"
 export const ProjectCard = ({p}) => {
   return (
     <div className="project-card-wrapper">
-      <Link to={"/project/" + p._id}>
+      <Link to={"/project/" + p.title}>
         <div className='project-card text-2xl'>
           <div className='card-details-wrapper'>
             <p className="font-bold">{p.title}</p>
             <div 
-            className='text-base pt-2'
+            className='text-base pt-2 pr-2'
             dangerouslySetInnerHTML={{ __html: p.description }} />
-            {/* <p>{p.gitLink}</p>
-            <p>{p.deployLink}</p> */}
+
           </div>
           <div className="project-card-images">
             <img className="rounded" src={p.images[0]} />
--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,6 +1,36 @@
+import { Link } from 'react-router-dom'
+import './Contact.css'
 const Contact = () => {
+
   return (
-    <h1>Contact</h1>
+    <div className='Contact'>
+      <div className='contact-header'>
+          <h1 className='p-4 font-bold text-3xl'>Contact</h1>
+      </div>
+      <div className="contact-container">
+        <a href="mailto:timhuitt@gmail.com">
+          <div className='contact'>
+              <button>
+                TimHuitt@gmail.com
+              </button>
+          </div>
+        </a>
+        <Link to="https://linkedin.com/in/timhuitt" target="_blank" rel="noopener noreferrer">
+          <div className='contact'>
+              <button>
+                LinkedIn.com/in/TimHuitt
+              </button>
+          </div>
+        </Link>
+        <Link to="https://github.com/TimHuitt" target="_blank" rel="noopener noreferrer">
+          <div className='contact'>
+              <button>
+                GitHub.com/TimHuitt
+              </button>
+          </div>
+        </Link>
+      </div>
+    </div>
   )
 }
 export default Contact
