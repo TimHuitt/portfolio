6238254bc9af038349201f8fd77d5109b463f6af
Tim Huitt <timhuitt@gmail.com>
2023-12-31 11:29:22 -0700

    update header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,15 @@ const App = () => {
 
   useEffect(() => {
     const root = document.documentElement;
+    const gradientLight = "linear-gradient(rgba(255, 255, 255, 1) 40%, rgba(255, 255, 255, 0) 90%)"
+    const gradientDark = "linear-gradient(rgba(28, 30, 41, 1) 30%, rgba(0, 0, 0, 0) 90%)"
     root.style.setProperty('--bg', isDark ? '#1c1e29' : '#f6f7f9')
     root.style.setProperty('--bg-dark', isDark ? '#131519' : '#ebebeb')
     root.style.setProperty('--bg-light', isDark ? '#272d37' : '#fafafa')
     root.style.setProperty('--shadow', isDark ? '#232323' : '#393d44')
     root.style.setProperty('--shadow-light', isDark ? '#282d35' : '#c4d0de')
     root.style.setProperty('--shadow-lighter', isDark ? '#7281a7' : '#aaa')
+    root.style.setProperty('--gradient-light', isDark ? gradientDark : gradientLight)
     root.style.setProperty('color', isDark ? '#ccc' : '#444')
   },[isDark])
 
