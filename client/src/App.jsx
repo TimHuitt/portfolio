db803839454d8fe24ad6f97eafadea91db25fb45
Tim Huitt <timhuitt@gmail.com>
2023-12-30 21:11:10 -0700

    update style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     root.style.setProperty('--bg-light', isDark ? '#272d37' : '#fafafa')
     root.style.setProperty('--shadow', isDark ? '#232323' : '#9a9a9a')
     root.style.setProperty('--shadow-light', isDark ? '#282d35' : '#c4d0de')
-    root.style.setProperty('--shadow-lighter', isDark ? '#8372a7' : '#aaa')
+    root.style.setProperty('--shadow-lighter', isDark ? '#7281a7' : '#aaa')
     root.style.setProperty('color', isDark ? '#ccc' : '#444')
   },[isDark])
 
