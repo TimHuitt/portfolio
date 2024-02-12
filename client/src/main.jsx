71631f2f65a7173510190ecc2186a67679790d58
Tim Huitt <timhuitt@gmail.com>
2024-02-12 12:23:54 -0700

    add analystics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,11 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from 'react-ga4'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+ReactGA.initialize("G-BQPMN865T2")
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
