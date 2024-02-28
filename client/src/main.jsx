e6e56060bcd235d80446cc3c0fcb31b0d21e3935
Tim Huitt <timhuitt@gmail.com>
2024-02-28 11:14:36 -0700

    update resume

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ReactGA from 'react-ga4'
 import { BrowserRouter as Router } from 'react-router-dom'
+import ReactGA from 'react-ga4'
 
 ReactGA.initialize("G-BQPMN865T2")
 
