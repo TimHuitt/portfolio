df4d300142ff202d9d87a461e0258ba43d452978
Tim Huitt <timhuitt@gmail.com>
2024-03-21 23:08:57 -0600

    update bio

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -49,17 +49,14 @@ const Home = () => {
           { showMore ? (
             <div className='about-more'>
               <p className='home-about about-2'>
-              Over the last decade, I have consistently utilized my initiative in professional settings to engineer more efficient systems, earning the trust of my peers and superiors to tackle critical technical challenges. The past five years have been especially transformative.
+              A graduate of General Assembly's Software Engineering bootcamp, I am well-versed in modern development practices and technologies.
               </p><br/>
               <p className='home-about about-3'>
-              A graduate of General Assembly's Software Engineering bootcamp, I am well-versed in modern development practices and technologies including React, Node.js, Django, and various database management systems. My approach combines creativity with efficiency, solving complex problems and leading projects to successful completion.
+              I have successfully applied my knowledge in a variety of settings, from small business projects to personal ventures, creating solutions for a wide range of needs including data visualization, file management, process optimization, IoT solutions, automation tools, and entertainment applications.
               </p><br/>
               <p className='home-about about-4'>
-              I have successfully applied my knowledge in a variety of settings, from small business projects to personal ventures, creating solutions for a wide range of needs including data visualization, file management, process optimization, IoT solutions, automation tools, and entertainment applications, each reinforcing my problem-solving skills and technical versatility.
+              My current interests and learning goals include  Web3, Blockchain, Smart Contracts, Machine Learning, Neural Networks, Cybersecurity, AWS, Azure, GCP, Docker, Kubernetes.
               </p><br/>
-              <p className='home-about about-5'>
-              I am excited about the prospect of bringing my unique blend of experience, passion, and commitment to your team. I am looking forward to contributing to innovative projects and collaborating with talented individuals to drive success and surpass expectations.
-              </p>
             </div>
           ) : (
             ''
