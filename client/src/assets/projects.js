0a5650a11a001f557665e1ee19ae0606b34ce5d2
Tim Huitt <timhuitt@gmail.com>
2024-05-11 17:52:12 -0600

    update all projects

--- client/src/assets/projects.js
+++ client/src/assets/projects.js
@@ -28,29 +28,25 @@ const projects = [
   },
   {
     "title": "Promptopus",
-    "description": "Prompt generator for artist inspiration. Artists can choose between Prompts or Stories. Prompts provide creativity prompts through randomly selected emojis or words. Stories provide random AI generated stories with length and theme options.",
+    "description": "Prompt generator for artist inspiration. Artists can choose between Prompts or Stories.",
     "longDescription": `
-    <p>Promptopus is an innovative and engaging app designed to inspire artists and creators by providing unique drawing prompts and storytelling ideas. This project showcases the integration of cutting-edge technologies with creative applications, offering users a platform to spark their imagination and creativity.</p>
-    
-    <h3>Overview</h3>
-    <p>Promptopus is built using Next.js, TypeScript, and OpenAI, demonstrating the latest in web development and artificial intelligence. This app is tailored for artists seeking inspiration, writers looking for story ideas, or anyone in need of a creative boost. Users can choose between generating random words or emojis for drawing prompts or receiving AI-generated stories based on customizable themes and lengths.</p>
-
-    <h3>Key Features</h3>
+    <p>Promptopus is an innovative and engaging app designed to inspire artists and creators by providing unique drawing prompts and storytelling ideas.</p>
+    <div>
+      <img src="https://i.imgur.com/8gaRKbp.png" />
+    </div>
+    <p>Promptopus is built using Next.js, TypeScript, and OpenAI, showcasing the latest in web development and artificial intelligence. Users can choose between generating random words or emojis for drawing prompts or receiving AI-generated stories based on customizable themes and lengths.</p>
+    <div>
+      <img src="https://i.imgur.com/8AOi92k.png" />
+    </div>
+    <h3 style="text-decoration: underline;">Features</h3>
     <ul>
-        <li><strong>Dynamic Prompt Generation:</strong> Users can generate unique drawing prompts by selecting between words or emojis, providing endless inspiration for creative projects.</li>
-        <li><strong>AI-Powered Story Generation:</strong> Leveraging OpenAI's capabilities, the app crafts stories in various lengths and themes, offering a rich source of inspiration for more detailed artworks or writing projects.</li>
-        <li><strong>Intuitive User Experience:</strong> A clean, user-friendly interface allows for seamless navigation and interaction, ensuring users can focus on unleashing their creativity.</li>
-        <li><strong>Responsive Design:</strong> The app is fully responsive, providing a consistent and accessible experience across all devices and screen sizes.</li>
-    </ul>
-
-    <h3>Technologies</h3>
-    <ul>
-        <li><strong>Next.js:</strong> Utilized for server-side rendering and static site generation, enhancing SEO and improving performance.</li>
-        <li><strong>TypeScript:</strong> Provides type safety and improves the development experience with static type checking.</li>
-        <li><strong>OpenAI API:</strong> Powers the AI-driven story and prompt generation, offering users creative and varied content.</li>
+        <li><strong>Dynamic Prompt Generation</strong><br/> Users can generate unique drawing prompts by selecting between words or emojis, providing endless inspiration.</li>
+        <li><strong>AI-Powered Story Generation</strong><br/> Leveraging OpenAI's capabilities, the app crafts stories in various lengths and themes.</li>
+        <li><strong>Intuitive User Experience</strong><br/> A clean, user-friendly interface allows for seamless navigation and interaction.</li>
+        <li><strong>Responsive Design</strong><br/> The app is fully responsive, providing a consistent experience across all devices.</li>
     </ul>
     `,
-    "images": ["https://i.imgur.com/8gaRKbp.png", "https://i.imgur.com/8AOi92k.png", "https://i.imgur.com/DoMvn3Z.png", "https://i.imgur.com/fzu6Xx3.png", "https://i.imgur.com/fpH8MtV.png"],
+    "images": ["https://i.imgur.com/8gaRKbp.png", "https://i.imgur.com/8AOi92k.png"],
     "gitLink": "https://github.com/TimHuitt/Promptopus",
     "deployLink": "https://promptopus.netlify.app/",
     "skills" : ['next', 'ts', 'express', 'node', 'html', 'css']
@@ -59,73 +55,96 @@ const projects = [
     "title": "Auto Component",
     "description": "An npm published tool that streamlines UI element creation in React projects through AI prompted element generation.",
     "longDescription": `
-    <p>AutoComponent is an innovative tool designed to transform the way UI elements are created in React projects. By integrating artificial intelligence with your development environment, AutoComponent speeds up the design and implementation of UI components, allowing developers to focus more on strategic tasks. This tool automates the generation of React components through a simple, user-friendly interface, enhancing productivity and reducing manual coding errors.</p>
-
-    <h2>Key Features</h2>
+    <p>AutoComponent is a developer tool published to npm and designed to transform the way UI elements are created in React projects. By integrating artificial intelligence with your development environment, AutoComponent speeds up the design and implementation of UI components.</p>
+    <div>
+      <img src="https://i.imgur.com/woDHKJY.png" />
+    </div>
+    <p>AutoComponent automates the generation of React components through a simple, user-friendly interface, enhancing productivity and reducing manual coding errors.</p>
+    <div>
+      <img src="https://i.imgur.com/qDJiLJe.png" />
+    </div>
+    <h3 style="text-decoration: underline;">Features</h3>
     <ul>
-        <li><strong>AI-Prompted Generation:</strong> Streamline the creation of React components with AI-driven prompts that understand your requirements and deliver precise code.</li>
-        <li><strong>Dynamic Rendering:</strong> Use JSXParser to dynamically render the generated code, ensuring what you see is what gets implemented.</li>
-        <li><strong>Code Beautification:</strong> Automatically format your JSX/HTML using js-beautify for improved readability and maintainability.</li>
-        <li><strong>Interactive UI:</strong> Engage with a user-friendly interface that allows for real-time interaction and modifications, facilitating a more intuitive development process.</li>
-    </ul>
-
-    <h2>Technologies Used</h2>
-    <ul>
-        <li><strong>Frontend:</strong> React, enabling dynamic UI development with JavaScript.</li>
-        <li><strong>Backend:</strong> A combination of Mongoose, Express, and Node.js provides a scalable and efficient server-side solution.</li>
-        <li><strong>Dependencies:</strong>
-            <ul>
-                <li><strong>Babel:</strong> Transpile JSX to JavaScript for broader compatibility and publishing.</li>
-                <li><strong>JSXParser:</strong> Render JSX dynamically, integrating generated code seamlessly into your project.</li>
-                <li><strong>js-beautify:</strong> Ensure that the generated UI code is not only functional but also cleanly formatted and easy to read.</li>
-            </ul>
-        </li>
+        <li><strong>AI-Prompted Generation</strong><br/> Streamline the creation of React components with AI-driven prompts that deliver precise code.</li>
+        <li><strong>Dynamic Rendering</strong><br/> Use JSXParser to dynamically render the generated code, ensuring accurate implementation.</li>
+        <li><strong>Code Beautification</strong><br/> Automatically formats JSX/HTML using js-beautify for improved readability.</li>
+        <li><strong>Interactive UI</strong><br/> Engage with a user-friendly interface that allows for real-time interaction and modifications.</li>
     </ul>
     `,
-    "images": ["https://i.imgur.com/woDHKJY.png", "https://camo.githubusercontent.com/8b10ffd215e9b1bd8f945c9c1fdb874ae2f639c72fca440d96edebad03a674c9/68747470733a2f2f692e696d6775722e636f6d2f446d58575064432e676966", "https://camo.githubusercontent.com/d43a391e7ffec538f06a4c312fd0492f71d1ba6a8484feec242145a062b0fbae/68747470733a2f2f692e696d6775722e636f6d2f7a5371783969502e676966", "w-https://i.imgur.com/qDJiLJe.png"],
+    "images": ["https://i.imgur.com/woDHKJY.png", "https://i.imgur.com/qDJiLJe.png"],
     "gitLink": "https://github.com/TimHuitt/auto-component",
     "deployLink": "https://auto-component.com/",
     "skills" : ['react', 'js', 'express', 'node', 'html', 'css']
   },
   {
     "title": "Green Diary",
-    "description": "Green Diary is an innovative CO2 tracking platform that helps you monitor the environmental impact of your vehicle trips. By understanding the carbon footprint of your travels, Green Diary aims to foster more eco-conscious decisions and behaviors.</a>",
+    "description": "An innovative CO2 tracking platform that helps monitor the environmental impact of your vehicle trips.",
     "longDescription": `
-    <h2>Features</h2>
+    <p>Green Diary is an innovative platform designed to help you monitor the environmental impact of your vehicle trips and foster more eco-conscious decisions.</p>
+    <div>
+      <img src="https://i.imgur.com/xurUff3.png" />
+    </div>
+    <p>By understanding the carbon footprint of your travels, Green Diary aims to encourage eco-friendly behaviors. Utilizes various APIs to prepopulate vehicle data for accurate emission tracking.</p>
+    <div>
+      <img src="https://i.imgur.com/YYFRFXu.png" />
+    </div>
+    <h3 style="text-decoration: underline;">Features</h3>
     <ul>
-      <li><strong>CO2 Emission Tracking:</strong> Calculates the amount of CO2 emitted on each trip based on your vehicle's make and model.</li>
-      <li><strong>Tree Mitigation Estimates:</strong> Provides insights into the number of trees required to offset your trip's carbon emissions.</li>
-      <li><strong>Vehicle Database:</strong> Utilizes various APIs to prepopulate vehicle makes and models for easy selection and accurate emission data.</li>
-      <li><strong>User-Friendly Interface:</strong> Built with Django and Bootstrap, the platform offers a responsive and intuitive user experience.</li>
-    </ul>
-
-    <h2>Technologies Used</h2>
-    <ul>
-      <li><strong>Django:</strong> A high-level Python Web framework that encourages rapid development and clean, pragmatic design.</li>
-      <li><strong>Django Authentication:</strong> For managing user accounts and authentication within the application.</li>
-      <li><strong>PostgreSQL:</strong> A powerful, open-source object-relational database system.</li>
-      <li><strong>Third-Party APIs:</strong> Integration of external APIs to fetch vehicle data and calculate emissions accurately.</li>
-      <li><strong>Bootstrap:</strong> For responsive design, ensuring the application is mobile-friendly and accessible on various devices.</li>
-      <li><strong>CSS:</strong> For styling the platform's web pages.</li>
-      <li><strong>HTML:</strong> For structuring the platform's web pages.</li>
+        <li><strong>CO2 Emission Tracking</strong><br/> Calculates the amount of CO2 emitted on each trip based on your vehicle's make and model.</li>
+        <li><strong>Tree Mitigation Estimates</strong><br/> Provides insights into the number of trees required to offset your trip's carbon emissions.</li>
+        <li><strong>User-Friendly Interface</strong><br/> Built with Django and Bootstrap, offering a responsive and intuitive experience.</li>
+        <li><strong>Comprehensive Vehicle Database</strong><br/> Utilizes extensive vehicle data for accurate emission calculations.</li>
     </ul>
     `,
-    "images": ["https://i.imgur.com/xurUff3.png", "https://i.imgur.com/CZm7h8i.png", "https://i.imgur.com/TCLAWoa.png", "w-https://i.imgur.com/YYFRFXu.png"],
+    "images": ["https://i.imgur.com/xurUff3.png", "https://i.imgur.com/YYFRFXu.png"],
     "gitLink": "https://github.com/elliesolhjou/Green-Diary",
     "deployLink": "https://green-diary-app-2098e92803f3.herokuapp.com/",
     "skills" : ['py', 'django', 'bootstrap', 'postgresql', 'html', 'css']
   },
   {
     "title": "pawzbe",
-    "description": "Pawzbe is a pet social media platform. User can create accounts using either Google or Github logins, and share photos and updates about their pets with their friends.<br><br><a class='link' href='https://pawzbe.com'>pawzbe.com</a>",
-    "images": ["https://i.imgur.com/Ct1IKgx.png", "https://i.imgur.com/6CN6238.png", "https://i.imgur.com/DJagbm4.png"],
+    "description": "Pawzbe is a pet social media platform where users can share photos and updates about their pets.",
+    "longDescription": `
+    <p>Pawzbe is a dedicated social media platform for pets and their owners. Users can create accounts and share updates about their pets with a community of pet lovers.</p>
+    <div>
+      <img src="https://i.imgur.com/Ct1IKgx.png" />
+    </div>
+    <p>User can create accounts using either Google or Github logins, engaging with friends and other pet enthusiasts.</p>
+    <div>
+      <img src="https://i.imgur.com/6CN6238.png" />
+    </div>
+    <h3 style="text-decoration: underline;">Features</h3>
+    <ul>
+        <li><strong>User-Friendly Social Interaction</strong><br/> Create a personal space for your pets and connect with other pet owners.</li>
+        <li><strong>Multiple Login Options</strong><br/> Sign in with Google or Github for quick access.</li>
+        <li><strong>Interactive Media Sharing</strong><br/> Share photos and updates, creating a vibrant pet-centric community.</li>
+    </ul>
+    `,
+    "images": ["https://i.imgur.com/Ct1IKgx.png", "https://i.imgur.com/6CN6238.png"],
     "gitLink": "https://github.com/TimHuitt/pawzbe",
     "deployLink": "https://pawzbe.com/",
     "skills" : ['js', 'express', 'node', 'mongo', 'html', 'css']
   },
   {
     "title": "Battleship!",
-    "description": "Advanced computer opponent<br /> Customizable ship placement<br /> Turn based tactical game play<br /> Game Stats<br /> Pop-up notifications",
+    "description": "Engage in naval warfare with advanced computer opponents and customizable ship placement in this turn-based tactical game.",
+    "longDescription": `
+    <p>Battleship! offers a classic naval combat experience where players can engage in strategic battles against an advanced AI opponent.</p>
+    <div>
+      <img src="https://i.imgur.com/ukgZLb4.png" />
+    </div>
+    <p>Enjoy customizable ship placement and tactical gameplay with pop-up notifications and game stats to track your progress.</p>
+    <div>
+      <img src="https://i.imgur.com/LOHaSfv.png" />
+    </div>
+    <h3 style="text-decoration: underline;">Features</h3>
+    <ul>
+        <li><strong>Advanced Computer Opponent</strong><br/> Challenge yourself against AI designed to test your strategic thinking.</li>
+        <li><strong>Customizable Ship Placement</strong><br/> Strategically place your fleet to outmaneuver your opponent.</li>
+        <li><strong>Turn-Based Gameplay</strong><br/> Take turns and make critical decisions to sink your opponent's ships.</li>
+        <li><strong>Game Stats and Notifications</strong><br/> Keep track of your victories and receive updates with in-game notifications.</li>
+    </ul>
+    `,
     "images": ["https://i.imgur.com/ukgZLb4.png", "https://i.imgur.com/LOHaSfv.png"],
     "gitLink": "https://github.com/TimHuitt/battleship",
     "deployLink": "https://timhuitt.github.io/battleship/",
@@ -133,8 +152,24 @@ const projects = [
   },
   {
     "title": "Tic Tac Toe",
-    "description": "Traditional two player Tic-Tac-Toe game with an updated UI. Features player turn/piece tracking and a resettable persistent score",
-    "images": ["https://i.imgur.com/ehk6k0f.png", "https://i.imgur.com/idWiPTd.png", "https://i.imgur.com/Q0PvA5m.png"],
+    "description": "Enjoy the classic two-player Tic-Tac-Toe game with modern UI enhancements, featuring turn/piece tracking and a resettable score.",
+    "longDescription": `
+    <p>Tic Tac Toe brings the traditional game to modern audiences with an updated user interface and enhanced gameplay features.</p>
+    <div>
+      <img src="https://i.imgur.com/ehk6k0f.png" />
+    </div>
+    <p>This game includes player turn/piece tracking and offers a resettable persistent score to keep the competition lively and engaging.</p>
+    <div>
+      <img src="https://i.imgur.com/idWiPTd.png" />
+    </div>
+    <h3 style="text-decoration: underline;">Features</h3>
+    <ul>
+        <li><strong>Updated User Interface</strong><br/> A fresh look for a classic game, enhancing the overall experience.</li>
+        <li><strong>Player Turn/Piece Tracking</strong><br/> Easily keep track of whose turn it is and what pieces are in play.</li>
+        <li><strong>Resettable Score</strong><br/> Reset scores to start a new series of games, keeping gameplay competitive and fair.</li>
+    </ul>
+    `,
+    "images": ["https://i.imgur.com/ehk6k0f.png", "https://i.imgur.com/idWiPTd.png"],
     "gitLink": "https://codepen.io/timhuitt/pen/NWoGKmo",
     "deployLink": "https://codepen.io/timhuitt/full/NWoGKmo",
     "skills" : ['js', 'html', 'css']
